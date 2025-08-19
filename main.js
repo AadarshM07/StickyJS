2249dc15d6f311870fb6f6d805a97eb787afdb00
Aadarsh <aadarshmahk@gmail.com>
2025-08-19 13:57:52 +0530

    updated js file

---
+++ main.js
@@ -0,0 +1,29 @@
+// script.js
+
+// 1. Get HTML elements from DOM
+
+
+// 2. Functions
+function getNotes() {
+    // TODO: Return array of notes from localStorage
+}
+
+function saveNotes(notes) {
+    // TODO: Save array of notes to localStorage
+}
+
+function displayNotes() {
+    // TODO: Render notes on the page
+}
+
+function deleteNote(id) {
+    // TODO: Delete a note by ID
+}
+
+// 3. Event listeners
+addNoteBtn.addEventListener("click", () => {
+    // TODO: Add new note
+});
+
+// 4. Initial load
+displayNotes();
